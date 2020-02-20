bee0d7b7240427166fa28513411bd7ff10c8f792
Tony Duan <tony@tonyduan.com>
2020-02-20 01:48:14 +0000

    Update attacks to return loss.

--- src/attacks.py
+++ src/attacks.py
@@ -29,7 +29,7 @@ def project_onto_ball(x, eps, p="inf"):
         raise ValueError("Can only project onto 1,2,inf norm balls.")
     return x.view(original_shape)
 
-def pgd_attack(model, x, y, eps, steps=20, p="inf", clamp=(0, 1)):
+def pgd_attack(model, x, y, eps, steps=20, adv="inf", clamp=(0, 1)):
     """
     Attack a model with PGD.
     """
@@ -40,60 +40,68 @@ def pgd_attack(model, x, y, eps, steps=20, p="inf", clamp=(0, 1)):
     for _ in range(steps):
         loss = model.loss(x, y).mean()
         grads = grad(loss, x)[0].reshape(x.shape[0], -1)
-        if p == 1:
+        if adv == 1:
             keep_vals = torch.kthvalue(grads.abs(), k=grads.shape[1] * 15 // 16, dim=1).values
             grads[torch.abs(grads) < keep_vals.unsqueeze(1)] = 0
             grads = torch.sign(grads)
             grads_norm = torch.norm(grads, dim=1, p=1)
             grads = grads / (grads_norm.unsqueeze(1) + 1e-8)
-        elif p == 2:
+        elif adv == 2:
             grads_norm = torch.norm(grads, dim=1, p=2)
             grads = grads / (grads_norm.unsqueeze(1) + 1e-8)
-        elif p == "inf":
+        elif adv == "inf":
             grads = torch.sign(grads)
         else:
             raise ValueError
         diff = x + step_size * grads.reshape(x.shape) - x_orig
-        diff = project_onto_ball(diff, eps, p)
+        diff = project_onto_ball(diff, eps, adv)
         x = (x_orig + diff).clamp(*clamp)
+
+    loss = model.loss(x, y).mean()
     x = x.detach()
     x.requires_grad = False
-    return x
+    return x, loss
 
-def pgd_attack_smooth(model, x, y, eps, noise, sample_size, steps=20, p="inf", clamp=(0, 1)):
+def pgd_attack_smooth(model, x, y, eps, noise, sample_size, steps=20, adv="inf", clamp=(0, 1)):
     """
     Attack a smoothed model with PGD.
     """
     step_size = 2 * eps / steps
     x.requires_grad = True
     x_orig = x.clone().detach()
+    rng = torch.cuda.get_rng_state_all()
 
     for _ in range(steps):
+        torch.cuda.set_rng_state_all(rng)
         forecast = smooth_predict_soft(model, x, noise, sample_size)
         loss = -forecast.log_prob(y).mean()
         grads = grad(loss, x)[0].reshape(x.shape[0], -1)
-        if p == 1:
+        if adv == 1:
             keep_vals = torch.kthvalue(grads.abs(), k=grads.shape[1] * 15 // 16, dim=1).values
             grads[torch.abs(grads) < keep_vals.unsqueeze(1)] = 0
             grads = torch.sign(grads)
             grads_norm = torch.norm(grads, dim=1, p=1)
             grads = grads / (grads_norm.unsqueeze(1) + 1e-8)
-        elif p == 2:
+        elif adv == 2:
             grads_norm = torch.norm(grads, dim=1, p=2)
             grads = grads / (grads_norm.unsqueeze(1) + 1e-8)
-        elif p == "inf":
+        elif adv == "inf":
             grads = torch.sign(grads)
         else:
             raise ValueError
         diff = x + step_size * grads.reshape(x.shape) - x_orig
-        diff = project_onto_ball(diff, eps, p)
+        diff = project_onto_ball(diff, eps, adv)
         x = (x_orig + diff).clamp(*clamp)
 #        forecast = smooth_predict_hard(model, x, noise, sample_size).probs
 #        print(_, (torch.argmax(forecast, dim=1) == y).sum() / float(x.shape[0]),
 #              diff.reshape(x.shape[0], -1).norm(dim=1, p=1).mean(),
 #              diff.reshape(x.shape[0], -1).norm(dim=1, p=2).mean())
 
+    torch.cuda.set_rng_state_all(rng)
+    forecast = smooth_predict_soft(model, x, noise, sample_size)
+    loss = -forecast.log_prob(y).mean()
+
     x = x.detach()
     x.requires_grad = False
-    return x
+    return x, loss
 
